facbc0b2f82c5b060f4b08381df5a8706e2a17d0
KasunAratthanage <kasunaratthanage@gmail.com>
2019-07-19 12:17:45 +0530

    Update deployement.sh file

--- deploymentRepository/ballerina_intgrator_deployement.sh
+++ deploymentRepository/ballerina_intgrator_deployement.sh
@@ -76,7 +76,8 @@ setup_deployment(){
 
 #Download the ballerina run script
 download_ballerina(){
-git clone https://github.com/ballerina-platform/ballerina-scenario-tests.git
+#git clone https://github.com/ballerina-platform/ballerina-scenario-tests.git
+git clone https://github.com/KasunAratthanage/ballerina-scenario-tests.git
 cd ballerina-scenario-tests/test-grid-scripts/setup
 source setup_deployment_env.sh
 echo "=== Ballerina Installed ==="
