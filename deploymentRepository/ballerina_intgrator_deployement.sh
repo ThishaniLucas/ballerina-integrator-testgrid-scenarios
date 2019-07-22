e2ee551b5a4918f2def55361d0707ef7d2ec938c
KasunAratthanage <kasunaratthanage@gmail.com>
2019-07-22 16:24:21 +0530

    Update deployement.sh

--- deploymentRepository/ballerina_intgrator_deployement.sh
+++ deploymentRepository/ballerina_intgrator_deployement.sh
@@ -120,14 +120,13 @@ cd examples/s3
 cp api_test.bal ../../../module-amazons3
 # to do --> need to add conf.bal here
 # ballerina build api_test.bal
+${ballerina_home}/bin/ballerina build api_test.bal
 cd ../../
 pwd
 # cd module-amazons3
 # touch conf.bal
 # echo "ballerina_integrator_aws_s3_access_key" >> /conf.bal
 # echo "ballerina_integrator_aws_s3_secret_key" >> /conf.bal
-
-${ballerina_home}/bin/ballerina build api_test.bal
 
 echo "=== Ballerina service build successfully ==="
 
