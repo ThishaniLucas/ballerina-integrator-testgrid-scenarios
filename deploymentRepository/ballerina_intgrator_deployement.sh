ace3f3bc1010c01d7e3120dae98c6ccfee646dec
KasunAratthanage <kasunaratthanage@gmail.com>
2019-07-22 17:23:55 +0530

    Update deployement.sh

--- deploymentRepository/ballerina_intgrator_deployement.sh
+++ deploymentRepository/ballerina_intgrator_deployement.sh
@@ -125,7 +125,7 @@ ls
 pwd
 cd module-amazons3
 touch ballerina.conf
-chmod -R 777 conf.bal
+chmod -R 777 ballerina.conf
 echo "ballerina_integrator_aws_s3_access_key" >> /ballerina.conf
 echo "ballerina_integrator_aws_s3_secret_key" >> /ballerina.conf
 ${ballerina_home}/bin/ballerina build api_test.bal
