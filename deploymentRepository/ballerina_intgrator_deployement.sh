ad50e8815470ff51c1cb843c29480f97f94a0704
KasunAratthanage <kasunaratthanage@gmail.com>
2019-07-23 17:49:44 +0530

    Upadet deployement.sh

--- deploymentRepository/ballerina_intgrator_deployement.sh
+++ deploymentRepository/ballerina_intgrator_deployement.sh
@@ -139,7 +139,8 @@ cd target/kubernetes
 # Run generated docker image
 kubectl apply -f ./api_test --namespace=${cluster_namespace}
 # kubectl apply -f /testgrid/testgrid-home/jobs/kasunA-ballerina-integrator-k8s/kasunA-ballerina-integrator-k8s_deployment_CentOS-7.5_MySQL-5.7_run67/workspace/DeploymentRepository/module-amazons3/target/kubernetes/api_test --namespace=${cluster_namespace}
-
+cd api_test/
+ls
 }
 
 write_properties_to_data_bucket() {
