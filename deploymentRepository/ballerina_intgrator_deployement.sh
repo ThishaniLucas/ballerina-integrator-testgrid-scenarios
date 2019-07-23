1d560864d8c8274e65f26825fca97d25e484d21e
KasunAratthanage <kasunaratthanage@gmail.com>
2019-07-23 11:54:38 +0530

    Upadet deployement.sh

--- deploymentRepository/ballerina_intgrator_deployement.sh
+++ deploymentRepository/ballerina_intgrator_deployement.sh
@@ -137,7 +137,10 @@ echo "=== Ballerina service build successfully ==="
 pwd
 ls
 # Run generated docker image
-kubectl apply -f /module-amazons3/target/kubernetes/api_test --namespace=${cluster_namespace}
+cd /target/kubernetes/
+pwd
+ls
+kubectl apply -f /api_test --namespace=${cluster_namespace}
 # kubectl apply -f /testgrid/testgrid-home/jobs/kasunA-ballerina-integrator-k8s/kasunA-ballerina-integrator-k8s_deployment_CentOS-7.5_MySQL-5.7_run67/workspace/DeploymentRepository/module-amazons3/target/kubernetes/api_test --namespace=${cluster_namespace}
 
 }
