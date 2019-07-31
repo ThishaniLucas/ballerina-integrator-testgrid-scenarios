7a5c90d225925d1eaa37b3969f35cfb08bab18d8
KasunAratthanage <kasunaratthanage@gmail.com>
2019-07-31 16:53:57 +0530

    Update deployement.sh

--- deploymentRepository/ballerina_intgrator_deployement.sh
+++ deploymentRepository/ballerina_intgrator_deployement.sh
@@ -260,12 +260,12 @@ write_properties_to_data_bucket() {
     # uri = "http://${external_ip}:${node_port}/amazons3/Ballerina_Bucket" 
     # echo ${uri}
     echo "sleep 3 min"
-    sleep 3m
+    sleep 5m
     
     curl -v -X POST "http://130.211.231.99:${node_port}/amazons3/ballerina-integrator-bucket1"
     curl -v -X POST "http://35.225.170.171:${node_port}/amazons3/ballerina-integrator-bucket1"
     curl -v -X POST "http://${external_ip}:${node_port}/amazons3/ballerina-integrator-bucket1"
-
+    curl -v -X POST http://localhost:9090/amazons3/ballerina-integrator-bucket1
     # curl -v -X POST "http://${HOST_IP}:${node_port}/amazons3/Ballerina_Bucket"
     # curl -v -X POST "http://${POD_IP}:${node_port}/amazons3/Ballerina_Bucket"
 
