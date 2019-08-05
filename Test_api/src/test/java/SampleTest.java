405e28ac1a4502cfc9527beb3c69194217846132
KasunAratthanage <kasunaratthanage@gmail.com>
2019-08-05 12:29:25 +0530

    Update SampleTest.java

--- Test_api/src/test/java/SampleTest.java
+++ Test_api/src/test/java/SampleTest.java
@@ -10,7 +10,6 @@ import java.io.InputStream;
 import java.util.Properties;
 
 
-
 import static io.restassured.RestAssured.given;
 
 public class SampleTest {
@@ -19,18 +18,20 @@ public class SampleTest {
 //    Path deployPropsFile = Paths.get(INPUTS_LOCATION + File.separator + DEPLOYMENT_PROPERTIES);//
 //    InputStream input = new FileInputStream((String)deployPropsFile))//
 //    Properties prop = new Properties();//
-//    // load a properties file//    prop.load(input);
+    //    // load a properties file//    prop.load(input);
 ////    // get the property value and print it out
 //    System.out.println(prop.getProperty("ExternalIP"));
 
 //    try
-//
-    (InputStream input = new FileInputStream("deployment.properties"))
-//    {
-        Properties props =new Properties();
-        props.load(input);
-        System.out.println(props.getProperty("ExternalIP"));
-        System.out.println(props.getProperty("NodePort"));
+//------------------------------------------------------
+//    (InputStream input = new FileInputStream("deployment.properties"))
+////    {
+//        Properties props =new Properties();
+//        props.load(input);
+//        System.out.println(props.getProperty("ExternalIP"));
+//        System.out.println(props.getProperty("NodePort"));
+
+        // --------------------------------------------------
 //    }
 
 //    catch(IOException ex)
@@ -38,26 +39,26 @@ public class SampleTest {
 //        ex.printStackTrace();
 //    }
 
-
     @BeforeTest
     public void init() throws Exception {
         try {
-
             //"http://0.0.0.0:9090/amazons3/"
-            RestAssured.baseURI = "http://ExternalIP:NodePort/amazons3/";
+            //ExternalIP:NodePort
+            RestAssured.baseURI = "http://0.0.0.0:9090/amazons3/";
         } catch (Exception e) {
             e.printStackTrace();
         }
     }
 
+    //Create bucket in the amazone s3
     @Test
     public void createbucket_Test() {
         Response response=
         given().
                 when().
-                post("ballerina-integrator-bucket2");
-                Assert.assertTrue(response.statusCode()==500);
-                Assert.assertTrue(response.body().asString().contains("Your previous request to create the named bucket succeeded and you already own it."));
+                    post("ballerina-integrator-bucket7");
+                Assert.assertTrue(response.statusCode()==200);
+                Assert.assertTrue(response.body().asString().contains("ballerina-integrator-bucket6 created from Amazon S3"));
 //        given().
 //        when().
 //        post();
@@ -65,6 +66,44 @@ public class SampleTest {
 //        assertThat()
 //        .statusCode(500);
     }
+
+
+
+//    @Test
+//    public void createObject_Test(){
+//        //curl -v -X POST --data @content.json http://localhost:9090/amazons3/firstbalbucket/firstObject.json --header "Content-Type:application/json"
+//        Response response=
+//
+//    }
+//
+//
+//    @Test
+//    public void getObject_Test(){
+//    //curl -v -X DELETE http://localhost:9090/amazons3/firstbalbucket/firstObject.json
+//
+//
+//    }
+//
+//
+//    @Test
+//    public void deleteObject_Test(){
+//        //curl -v -X DELETE http://localhost:9090/amazons3/firstbalbucket
+//
+//
+//    }
+
+    //Delete bucket in the amazone s3
+
+    @Test (dependsOnMethods = {"createbucket_Test"})
+    public void deletebucket_Test() {
+        Response response=
+                given().
+                        when().
+                        post("ballerina-integrator-bucket6");
+        Assert.assertTrue(response.statusCode()==200);
+        Assert.assertTrue(response.body().asString().contains("ballerina-integrator-bucket6 deleted from Amazon S3."));
+    }
+
 
 //    @Test(dependsOnMethods = {"createbucket_Test"})
 //    public void deletebucket_Test() {
